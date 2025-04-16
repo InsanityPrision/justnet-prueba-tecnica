5707af02d02ecb6c6523c7cfed7030c5fbb6e440
Erik Riquelme <erikone2311@gmail.com>
2025-04-16 19:38:09 +0200

    Apply css to NavMenu and UsersPage for better colocation

--- src/users/pages/UsersPage/UsersPage.tsx
+++ src/users/pages/UsersPage/UsersPage.tsx
@@ -1,9 +1,10 @@
 import UsersList from "../../components/UsersList/UsersList";
+import "./UsersPage.css";
 
 const UsersPage: React.FC = () => {
   return (
     <>
-      <h1>Usuarios</h1>
+      <h1 className="page-title">Usuarios</h1>
       <UsersList users={[]} />
     </>
   );
