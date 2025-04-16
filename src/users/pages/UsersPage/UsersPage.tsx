ea3d2c3f827a04105c123084473030afda6921e5
Erik Riquelme <erikone2311@gmail.com>
2025-04-16 20:38:46 +0200

    Add functionality in UserPage for load all users from the API

--- src/users/pages/UsersPage/UsersPage.tsx
+++ src/users/pages/UsersPage/UsersPage.tsx
@@ -1,11 +1,30 @@
+import { useCallback, useEffect, useState } from "react";
 import UsersList from "../../components/UsersList/UsersList";
 import "./UsersPage.css";
+import { usersClient } from "../../client/UsersClient";
+import User from "../../types";
 
 const UsersPage: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  const loadUsers = useCallback(async () => {
+    try {
+      const loadedUsers = await usersClient.getUsers();
+
+      setUsers(loadedUsers);
+    } catch {
+      throw new Error("Error loading Users");
+    }
+  }, []);
+
+  useEffect(() => {
+    loadUsers();
+  }, [loadUsers]);
+
   return (
     <>
       <h1 className="page-title">Usuarios</h1>
-      <UsersList users={[]} />
+      <UsersList users={users} />
     </>
   );
 };
